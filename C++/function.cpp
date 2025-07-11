c32c5f1a2853760eb158e1b2c6444a8e295da6e1
unknown <ngudeta87@gmail.com.com>
2025-07-11 03:52:59 -0700

    function

--- C++/function.cpp
+++ C++/function.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
  void  celsius_to_fahranheit(double cel){
  	double F;
- 	F=(cel*1.8)+32;
+ 	F=(cel*1.8)+32 ;
  	cout<<F<<endl;
+ }
+ 	
+void  fahranheit_to_celsius(double far){
+ 	double C;
+    C=9/5*(far-32);
+ 	cout<<C<<endl;
  	
  }
  
  int main(){
+celsius_to_fahranheit(0);
+ 	celsius_to_fahranheit(32);
  	celsius_to_fahranheit(0);
+ 	celsius_to_fahranheit(32);
  		return 0;
 }
  
