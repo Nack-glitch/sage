9ebb664a2b8b9a82f64871030ff14c1e7a6d48a8
unknown <ngudeta87@gmail.com.com>
2025-07-11 03:42:04 -0700

    function

---
+++ C++/function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+ void  celsius_to_fahranheit(double cel){
+ 	double F;
+ 	F=(cel*1.8)+32;
+ 	cout<<F<<endl;
+ 	
+ }
+ 
+ int main(){
+ 	celsius_to_fahranheit(0);
+ 		return 0;
+}
+ 
