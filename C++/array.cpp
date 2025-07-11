d8bd0e58bebe54dca7b1f708431320124748e1b6
unknown <ngudeta87@gmail.com.com>
2025-07-11 03:17:52 -0700

    c++

---
+++ C++/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+char vowels[5]={'a','o','u','i','e'};
+int i=0;
+cout<<"vowels ";
+while(i<5){
+	cout<<vowels[i]<<" ";
+	i++;
+}
+return 0;
+}
---
+++ C++/f.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double c,f;
+	cout<<"enter your c-";
+	cin>>c;
+	f=(c*1.8)+32;
+	cout<<"f"<<f<<endl;
+}
