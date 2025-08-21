5c69b1af20b8ad4abc56c8f4c0dcf54ab0281ea5
Nack gltich <ngudeta87gmail.com>
2025-08-21 04:02:09 -0700

    node

--- React/fetch/src/App.js
+++ React/fetch/src/App.js
@@ -1,140 +1,43 @@
-
-import './App.css';
-import { useState } from 'react';
-
+import { useState } from "react"
 
 export default function App() {
-  const[country,setCountry]=useState("")
-  function getcountry(){
-    fetch("https://restcountries.com/v3.1/all")
-    .then((res)=>res.json())
-    .the((data)=>setCountry(data))
-  }
-  const[search,setSearch]=useState("")
-  function search(name){
-    fetch ("https://restcountries.com/v3.1/name/"+ name)
-    .then((res)=>
-      {if(res.status==200)
-        return  res.json()
-        else return []
-      })
-    
-    .then((data)=>setCountry(data))                                                                                                                                                                                                                                                                                                                                                                                    
-  }
-  return <>
- <input placeholder='search a capital city' type='search'
-  value={search} 
-  onChange={(e)=>setSearch(e.target.value)}/>
-  
-<button onClick={()=>search(search) }>capital</button>
-<button onClick={()=>getcountrys() }>capital</button>
+    const [countrys, setCountrys] = useState([])
+    const [searchCountryName, setSearchCountryName] = useState("")
+    function getCountrys() {
+        fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+            .then((res) => res.json())
+            .then((data) => setCountrys(data))
+    }
+    function search(name) {
+        fetch("https://restcountries.com/v3.1/name/" + name)
+            .then((res) => {
+                if (res.status == 200)
+                    return res.json()
+                else
+                    return []
+            })
+            .then((data) => setCountrys(data))
+    }
+    return (
+        <>
+            <h1> country list </h1>
+            <input placeholder="search" type="search"
+                value={searchCountryName}
+                onChange={(e) => setSearchCountryName(e.target.value)}
+            />
+            <button onClick={() => search(searchCountryName)}>Search Country</button>
+            <button onClick={() => getCountrys()}>Get Countrys</button>
+            <div style={{ display: "flex", gap: "15px", maxWidth: "100%", flexWrap: "wrap", padding: "10px" }}>
+                {countrys.map((country) => (
+                    <div style={{backgroundColor:"red"}} >
+                        <img src={country.flags.png} alt={country.flags.alt} width="100%" />
+                        <p>country common name: {country.name.common} </p>
+                        <p>country official name: {country.name.official}</p>
+                    </div>
+                ))}
 
-<div >
-{country.map((country)=>(
-  <div>
-    <img src={country.flags.png} alt={country.flags.png}  width="100%"/>
-    <p>common name: {country.name.common}</p>
-    <p>official name: {country.name.official}</p>
+        </div>
 
-  </div>
-))}
-</div>
-  
-
-  </>  
+        </>
+    )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const fs =require('fs');
+fs.readFile("output.txt",'utf8',(err,data)=>{
+    if(err) throw err
+    console.log(data)
+})
+
+fs.writeFile("writeFileExample.csv","name,age,gender",(err)=>{
+    if(err) throw err
+    console.log ("completed")
+})
